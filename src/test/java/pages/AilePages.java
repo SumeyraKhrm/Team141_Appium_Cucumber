b706f662cfb74585e7fca03a23955dab3969837e
SumeyraKhrm <sumeyra.khrm@outlook.com>
2024-10-12 10:35:21 +0200

    v

---
+++ src/test/java/pages/AilePages.java
@@ -0,0 +1,120 @@
+package pages;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utils.ConfigReader;
+import utils.Driver;
+import utils.ReusableMethods;
+
+import java.time.Duration;
+
+public class AilePages {
+
+    public AilePages(){
+        PageFactory.initElements(new AppiumFieldDecorator(Driver.getAndroidDriver(),Duration.ofSeconds(15)),this);
+    }
+
+    @FindBy(xpath = "(//*[@class='android.widget.EditText'])[1]")
+    private MobileElement emailBox;
+
+    @FindBy(xpath = "(//*[@class='android.widget.EditText'])[2]")
+    private MobileElement passwordBox;
+
+    @FindBy(xpath ="//*[@text='Başarıyla giriş yapıldı.']") //
+    public MobileElement basariliGirisText;
+    @FindBy(xpath = "(//*[@class='android.widget.ImageView'])[1]")
+    public MobileElement hamburgerMenu;
+
+    @FindBy(xpath = "(//*[@class='android.widget.EditText'])[1]")
+    public MobileElement isimKutusu;
+
+    @FindBy(xpath = "(//*[@class='android.widget.EditText'])[2]")
+    public MobileElement soyisimKutusu;
+
+    @FindBy(xpath = "(//*[@class='android.widget.EditText'])[3]")
+    public MobileElement sehirKutusu;
+
+    @FindBy(xpath = "(//*[@class='android.widget.EditText'])[4]")
+    public MobileElement yasKutusu;
+
+    @FindBy(xpath = "(//*[@class='android.widget.EditText'])[5]")
+    public MobileElement meslekKutusu;
+
+    @FindBy(xpath = "(//*[@class='android.widget.EditText'])[1]")
+    public MobileElement aciklamaKutusu;
+
+    @FindBy(xpath = "(//*[@class='android.widget.EditText'])[2]")
+    public MobileElement tutarKutusu;
+
+    @FindBy(xpath = "(//*[@class='android.view.ViewGroup'])[10]")
+    public MobileElement gelirGiderTipi;
+
+    @FindBy(xpath = "//*[@text='Kategori']")
+    public MobileElement kategoriTipi;
+
+    @FindBy(xpath = "(//*[@class='android.widget.ImageView'])[5]")
+    public MobileElement tarih;
+
+    @FindBy(id = "android:id/next")
+    public MobileElement nextMonth;
+
+    @FindBy(xpath = "//*[@text='Gelir eklendi.']")
+    public MobileElement gelirEklendiText;
+
+    @FindBy(xpath = "//*[@text='Gider eklendi.']")
+    public MobileElement giderEklendiText;
+
+    public void aySecmeMethodu(int gidilecekAY,String secilecekGun) throws InterruptedException {
+        for (int i = 0; i <gidilecekAY ; i++) {
+            nextMonth.click();
+            Thread.sleep(1000);
+        }
+        ReusableMethods.scrollWithUiScrollableAndClick(secilecekGun);
+        ReusableMethods.scrollWithUiScrollableAndClick("OK");
+    }
+
+
+    public void hesabimKutuTemizleme(){
+        isimKutusu.clear();
+        soyisimKutusu.clear();
+        sehirKutusu.clear();
+        yasKutusu.clear();
+        meslekKutusu.clear();
+    }
+
+    public void hesabimYeniBilgiDogrulama(String isim,String soyIsim,String sehir,String yas,String meslek){
+        hesabimKutuTemizleme();
+        isimKutusu.sendKeys(ConfigReader.getProperty(isim));
+        soyisimKutusu.sendKeys(ConfigReader.getProperty(soyIsim));
+        sehirKutusu.sendKeys(ConfigReader.getProperty(sehir));
+        yasKutusu.sendKeys(ConfigReader.getProperty(yas));
+        meslekKutusu.sendKeys(ConfigReader.getProperty(meslek));
+        ReusableMethods.scrollWithUiScrollableAndClick("Kaydet");
+        Assert.assertEquals(isimKutusu.getText(), ConfigReader.getProperty(isim));
+        Assert.assertEquals(soyisimKutusu.getText(),ConfigReader.getProperty(soyIsim));
+        Assert.assertEquals(sehirKutusu.getText(), ConfigReader.getProperty(sehir));
+        Assert.assertEquals(yasKutusu.getText(), ConfigReader.getProperty(yas));
+        Assert.assertEquals(meslekKutusu.getText(), ConfigReader.getProperty(meslek));
+    }
+
+
+
+
+    public void loginMethodu(String email,String password){ // bilgilerle login girişi dinamik
+        emailBox.sendKeys(ConfigReader.getProperty(email));
+        passwordBox.sendKeys(ConfigReader.getProperty(password));
+        ReusableMethods.scrollWithUiScrollableAndClick("Giriş Yap");
+    }
+    public void ilkEkranAyarlari(int xPress,int yPress, int bekleme,int xMove,int yMove,String girisYap) throws InterruptedException {
+        Driver.getAndroidDriver();
+        Thread.sleep(2000);
+        for (int i = 0; i < 6; i++) {
+            ReusableMethods.ekranKaydirmaMethodu(xPress,yPress,bekleme,xMove,yMove);
+        }
+        ReusableMethods.scrollWithUiScrollableAndClick(girisYap);
+    }
+}
---
+++ src/test/java/stepDefinitions/aileButcem.java
@@ -0,0 +1,96 @@
+package stepDefinitions;
+
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
+import io.cucumber.java.en.Given;
+import org.junit.Assert;
+import pages.AilePages;
+import utils.Driver;
+import utils.ReusableMethods;
+
+import java.time.Duration;
+
+public class aileButcem {
+    AilePages pages=new AilePages();
+
+
+    @Given("ilk ekran ayarlarini yapin {int} {int} {int} {int} {int} ve ardindan login sayfasina {string} ulasin")
+    public void ilk_ekran_ayarlarini_yapin_ve_ardindan_login_sayfasina_giris_yap_ulasin(int x,int y,int bekleme,int moveX,int moveY,String girisYapText) throws InterruptedException {
+        pages.ilkEkranAyarlari(x,y,bekleme,moveX,moveY,girisYapText);
+    }
+    @Given("{string} ve {string} bilgilerini girerek kullanici bilgileriyle giris yapin")
+    public void mail_ve_sifre_bilgilerini_girerek_kullanici_bilgileriyle_giris_yapin(String mail,String sifre) {
+        pages.loginMethodu(mail,sifre);
+    }
+    @Given("uygulamaya kullanici bilgileriyle giris yapildigini dogrulayin")
+    public void uygulamaya_kullanici_bilgileriyle_giris_yapildigini_dogrulayin() {
+        Assert.assertTrue(pages.basariliGirisText.isDisplayed());
+    }
+    @Given("sol kisimdaki menuden {string} bolumune gidin")
+    public void sol_kisimdaki_menuden_hesabım_bolumune_gidin(String menuSecim) throws InterruptedException {
+        Thread.sleep(4000);
+        pages.hamburgerMenu.click();
+        ReusableMethods.scrollWithUiScrollableAndClick(menuSecim);
+    }
+    @Given("hesabim sayfasindaki bilgileri {string} {string} {string} {string} {string} degistirerek degisikleri kaydedin ve dogrulayin")
+    public void hesabim_sayfasindaki_bilgileri_degistirerek_degisikleri_kaydedin_ve_dogrulayin(String isim,String soyIsim,String sehir,String yas,String meslek) throws InterruptedException {
+        pages.hesabimYeniBilgiDogrulama(isim,soyIsim,sehir,yas,meslek);
+        Thread.sleep(2000);
+    }
+    @Given("Kullanici uygulamayi kapatir")
+    public void kullanici_uygulamayi_kapatir() {
+        Driver.quitAppiumDriver();
+    }
+
+
+    @Given("anasayfadaki arti butonuna tiklayin")
+    public void anasayfadaki_arti_butonuna_tiklayin() throws InterruptedException {
+        ReusableMethods.koordinatTiklamaMethodu(536,2040,500);
+    }
+    @Given("{string} Butonuna text uzerinden Tiklanir")
+    public void butonuna_text_uzerinden_tiklanir(String secenek) {
+        ReusableMethods.scrollWithUiScrollableAndClick(secenek);
+    }
+    @Given("Gelir Ekle sayfasinda aciklama kismina {string} deger girilir")
+    public void gelir_ekle_sayfasinda_aciklama_kismina_ıkinci_gelir_degeri_deger_girilir(String aciklama) {
+        pages.aciklamaKutusu.sendKeys(aciklama);
+    }
+    @Given("Gelir Ekle sayfasinda Gelir tipi {string} secilir")
+    public void gelir_ekle_sayfasinda_gelir_tipi_düzenli_secilir(String gelirTipi) throws InterruptedException {
+        pages.gelirGiderTipi.click();
+        Thread.sleep(1000);
+        ReusableMethods.scrollWithUiScrollableAndClick(gelirTipi);
+    }
+
+    @Given("Gelir Ekle sayfasinda Kategori {string} secilir")
+    public void gelir_ekle_sayfasinda_kategori_kategori_maaş_geliri_secilir(String kategori) throws InterruptedException {
+        pages.kategoriTipi.click();
+        Thread.sleep(1000);
+        ReusableMethods.scrollWithUiScrollableAndClick(kategori);
+    }
+    @Given("Gelir Ekle sayfasinda Tarih belirlemesi {int} ve gun secimi {string} yapilir")
+    public void gelir_ekle_sayfasinda_tarih_belirlemesi_ve_gun_secimi_yapilir(int gidilecekAy,String secilecekGun) throws InterruptedException {
+        pages.tarih.click();
+        Thread.sleep(1000);
+        pages.aySecmeMethodu(gidilecekAy,secilecekGun);
+    }
+    @Given("Gelir Ekle sayfasinda Tutar bilgisi {string} girilir")
+    public void gelir_ekle_sayfasinda_tutar_bilgisi_girilir(String tutar) {
+        pages.tutarKutusu.sendKeys(tutar);
+    }
+
+    @Given("basariyla eklendigini dogrulayin")
+    public void basariyla_eklendigini_dogrulayin() {
+        Assert.assertTrue(pages.gelirEklendiText.isDisplayed());
+    }
+    @Given("Gelir Ekle sayfasinda Gelir Periyodu Gelir tipi Aylık secilir")
+    public void gelir_ekle_sayfasinda_gelir_periyodu_gelir_tipi_aylık_secilir() {
+
+    }
+
+    @Given("basariyla gider eklendigini dogrulayin")
+    public void basariyla_gider_eklendigini_dogrulayin() {
+        Assert.assertTrue(pages.giderEklendiText.isDisplayed());
+    }
+}
